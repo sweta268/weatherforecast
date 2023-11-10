cd0c08001b3f17d75e5adb4cc9cf4fa304b0c82f
sweta268 <sweta.purohitbhatt@outlook.com>
2023-11-10 10:38:10 -0500

    Current Weather logic put in place

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -13,16 +13,45 @@ import { CommonModule } from '@angular/common';
 export class SearchComponent {
   cityName: string = '';
   WeatherData: any;
+  feelsLike:any;
+  pressure:any;
+  temp:any;
+  temp_max:any;
+  temp_min:any;
+
   constructor(private router: Router,private weatherService: WeatherService, private httpClient: HttpClient) {}
 
   searchCity() {
     if (this.cityName) {
-      console.log(this.cityName);
-        this.weatherService.getWeather(this.cityName).subscribe(data => {
-          this.WeatherData = data;
+           this.weatherService.getWeather(this.cityName).subscribe(data => {
+           this.WeatherData = data;
+           
+           if(this.WeatherData && this.WeatherData.main &&this.WeatherData.main.feels_like)
+           {
+            
+            this.feelsLike=(((parseFloat(this.WeatherData.main.feels_like) - 273.15) * 1.8) + 32 ).toFixed(2)//converting Kelvin to Farenheit
+          }
+          if(this.WeatherData && this.WeatherData.main && this.WeatherData.main.pressure)
+          {
+           this.pressure=(((parseFloat(this.WeatherData.main.pressure ) - 273.15) * 1.8) + 32 ).toFixed(2)//converting Kelvin to Farenheit
+          
+         }if(this.WeatherData && this.WeatherData.main &&this.WeatherData.main.temp)
+         {
+          this.temp=(((parseFloat(this.WeatherData.main.temp) - 273.15) * 1.8) + 32 ).toFixed(2)//converting Kelvin to Farenheit
+        }
+        if(this.WeatherData && this.WeatherData.main &&this.WeatherData.main.temp_max)
+         {
+          this.temp_max=(((parseFloat(this.WeatherData.main.temp_max) - 273.15) * 1.8) + 32).toFixed(2) //converting Kelvin to Farenheit
+        }
+        if(this.WeatherData && this.WeatherData.main &&this.WeatherData.main.temp_min)
+         {
+          this.temp_min=(((parseFloat(this.WeatherData.main.temp_min) - 273.15) * 1.8) + 32).toFixed(2) //converting Kelvin to Farenheit
+        }
           console.log(this.WeatherData);
         // this.router.navigate(['/forecast', this.cityName]);
       });
   }
   }
 }
+
+
